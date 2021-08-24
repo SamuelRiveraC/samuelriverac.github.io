9a3ad879f6f3143def36d100c335cc33a75fd3bc
RIVERA B. MIGUEL <RIVERAMR@XMOBILE68A70E.ori.pdvsa.com>
2021-08-24 10:16:57 -0400

    File names

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Fade from 'react-reveal/Fade';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 
 import Portfolios from "../components/Portfolios"
 import Testimonials from "../components/Testimonials"
